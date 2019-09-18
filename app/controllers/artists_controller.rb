b867e5d0e5bac69e1a1b40de04133e039d964655
Tracy Reuther <tracyreuther@gmail.com>
2019-09-18 23:44:56 +0000

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -3,7 +3,8 @@ class ArtistsController < ApplicationController
 #This should present the user with a list of all artists in the library.
 #Each artist should be a clickable link to that particular artist's show page.
   get '/artists' do
-
+    @artists = Artist.all
+    erb :'artists/index'
   end
 
 #Any given artist's show page should have links to each of his or her
--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -6,11 +6,17 @@ class SongsController < ApplicationController
 #This should present the user with a list of all songs in the library.
 #Each song should be a clickable link to that particular song's show page.
   get '/songs' do
+    @songs = Song.all
 
+    erb :'/songs/index'
   end
 
+#Be able to create a song
+#Genres should be presented as checkboxes
+#Be able to enter the Artist's name in a text field (only one Artist per song.)
   get '/songs/new' do
 
+    erb :'/songs/new'
   end
 
 #Other routes go in here...
@@ -19,6 +25,12 @@ class SongsController < ApplicationController
 #each genre associated with the song.
 #
   get '/songs/:slug' do
+
+  end
+
+#Be able to change everything about a song, including the genres associated
+#with it and its artist.
+  get '/songs/:slub/edit' do
 
   end
 end
