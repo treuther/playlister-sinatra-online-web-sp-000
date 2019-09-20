0794fbd324282219e4d53312563bb5155bf58fc0
Tracy Reuther <tracyreuther@gmail.com>
2019-09-20 14:19:55 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -3,4 +3,13 @@ class Artist < ActiveRecord::Base
   extend Slugifiable::ClassMethods
   has_many :songs
   has_many :genres, :through => :songs
+
+  def slug
+    self.name.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
+  end
+
+  def find_by_slug(slug)
+  self.all.find{|instance| instance.slug == slug}
+  end
+  
 end
